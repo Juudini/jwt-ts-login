f32a13aa04e517c3b80dc7364d18e715e25ee769
Juudini <juudinidev@gmail.com>
2023-11-02 17:00:51 -0300

    refactor(env.config.ts): Changed 'DB_PORT' to 'DB_PORT_DOCKER'

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -4,9 +4,10 @@ import { get } from "env-var";
 export const envs = {
     MODE: get("MODE").required().asString(),
     PORT: get("PORT").required().asPortNumber(),
+    JWT_SEED: get("JWT_SEED").required().asString(),
     DB_HOST: get("DB_HOST").required().asString(),
     DB_USER: get("DB_USER").required().asString(),
     DB_PASSWORD: get("DB_PASSWORD").required().asString(),
     DB_DATABASE: get("DB_DATABASE").required().asString(),
-    DB_PORT: get("DB_PORT").required().asPortNumber()
+    DB_PORT_DOCKER: get("DOCKER_DB_PORT").required().asPortNumber()
 };
